cd1cba66ef4dacac520830cd3e511effe06e0a4e
Mayank Kandpal <119619819+mynk1612@users.noreply.github.com>
2026-03-14 18:56:41 +0000

    variables, dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 144553
+let accountEmail = "mayank@google.com"
+var accountPassword = "123456789"
+accountCity = "Jaipur"
+let accountState = "UK";
+let accountNumber ;
+// accountId = 2 // not allowed
+
+// for (let index = 0; index < array.length; index++) {
+    // const element = array[index];
+// }
+
+accountEmail = "dada.com"
+accountPassword = "12451222"
+accountCity = "Bengaluru"
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and funcitonal scope
+*/
+
+
+console.log([accountId, accountEmail, accountPassword, accountCity, accountState, accountNumber])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all JS code as newer version
+
+// alert('hello') // we are using nodejs , not brower 
+console.log(30 + 3);
+console.log(" Mayank ");
+
+// console.log(30 
+// +
+//  3);  // code readability should be high 
+
+// DATATYPES 
+
+let name = "mayank" // string datatype 
+let age = 12 // int datatype 
+let isLoggedIn = false // boolean datatype 
+
+// number => 2 to the power 53
+// bigint 
+// string '' , "" 
+// boolean  =>  true/false 
+// null   =>  /*stand alone value */ 
+
+// undefined => value not defined 
+// symbol => unique 
+
+
+
+// object 
+
+console.log(typeof "Mayank"); // string 
+console.log(typeof age); // number 
+console.log(typeof null); // object 
+console.log(typeof undefined); // undefined 
+
+
+
