1466550a1d03c5f80868bd054e47a6e09f7d4413
Mayank Kandpal <119619819+mynk1612@users.noreply.github.com>
2026-03-29 20:27:01 +0000

    one

---
+++ 04_controlflow/one.js
@@ -0,0 +1,28 @@
+// LOGIC FLOW 
+// if statement
+
+const isUserloggedIn = true // single operator(=) used for asssign  
+const temperature = 60 
+
+if (temperature === 50 ){
+    console.log("less than 50");
+} else {
+    console.log("temperature is greater than 50");
+}
+
+
+// <, >, <=, 2<=2, >=, ==, !=, ===, !==(triple equal)
+
+// +++++++++++++ IF STATEMENT EXAMPLE ++++++++++
+/*
+if(false){
+} // code not execute
+<, >, <=, 2<=2, >=, ==, !=, ===, !==(triple equal)  // comparison 
+
+if (isUserLoggedIn){
+}
+
+if (2 !== "2"){
+    console.log("executed");
+}
+    */
