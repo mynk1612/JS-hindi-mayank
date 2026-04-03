aa57d4b75ffd88d8c0a0af3e80a7565f39fa56f3
Mayank Kandpal <119619819+mynk1612@users.noreply.github.com>
2026-04-03 04:18:38 +0000

    three

---
+++ 05_loops_iterations/three.js
@@ -0,0 +1,36 @@
+// FOR OF LOOP..........array specific loop
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings = "HELLO WORLD" // strings
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`);  
+}
+
+// MAPS 
+
+const map = new Map()
+map.set('IN', "India")
+map.set('NW', "Newzealand")
+map.set('Fr', "france")
+console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+/*
+const myObject = {
+    'game1': 'NFS',
+    'game2': 'Spiderman'
+}
+
+for (const [key, value] of myObject) {
+    console.log(key, ':-', value);
+}*/ // error
