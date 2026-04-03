b7a7ba7a1493f1a86af39e019c6fa4c1371f5eed
Mayank Kandpal <119619819+mynk1612@users.noreply.github.com>
2026-04-03 04:15:23 +0000

    switch

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,52 @@
+/*  +++++ BASIC CODE FOR SWITCH+++++
+switch (key) {
+    case value:
+
+    break;
+
+    default:
+        break;
+}*/
+
+/*  for number 
+const month = 3 
+
+switch (month) {
+    case 1:
+        console.log("January")
+    break;
+    case 2:
+        console.log("February")
+    break;
+    case 3:
+        console.log("March")
+     break;
+    case 4:
+        console.log("April")
+    break;
+
+    default:
+        console.log("default case match");
+        break;
+}*/
+
+// FOR STRING
+const month = "April"
+switch (month) {
+    case "January":
+        console.log("January")
+    break;
+    case "February":
+        console.log("February")
+    break;
+    case "March":
+        console.log("March")
+     break;
+    case "April":
+        console.log("April")
+    break;
+
+    default:
+        console.log("default case match");
+        break;
+}
