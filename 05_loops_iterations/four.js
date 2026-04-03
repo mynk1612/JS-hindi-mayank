d17fe7f2c7f32310c0e0824d4a8a66399753b89c
Mayank Kandpal <119619819+mynk1612@users.noreply.github.com>
2026-04-03 04:19:13 +0000

    four

---
+++ 05_loops_iterations/four.js
@@ -0,0 +1,29 @@
+// object 
+
+const myObject = {
+    js : 'javascript',
+    cpp : 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+       // console.log(myObject[key]);
+        // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+// for in for Array 
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+   console.log(programming[key]);
+
+}
+/*
+const map = new Map()
+map.set('IN', "India")
+map.set('NW', "Newzealand")
+map.set('Fr', "france")
+for (const key in map) {
+    console.log(key);
+}*/
