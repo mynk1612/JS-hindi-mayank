ac6549f24e5ce02763afb6cab9d8317b9e1a532d
Mayank Kandpal <119619819+mynk1612@users.noreply.github.com>
2026-04-25 09:02:30 +0000

    promise

--- 09_advanced_one/promises.js
+++ 09_advanced_one/promises.js
@@ -1,2 +1,13 @@
 //fetch('https://google.com').then().catch().finally()
-const promiseOne = new Promise()  
+const promiseOne = new Promise(function(resolve, reject){
+    // Do an async task
+    // DB calls, cryptography related , network 
+
+    setTimeout(function(){
+        console.log('Async task is complete');
+    }, 1000)
+})  
+
+promiseOne.then(function(){     // then-- connection wth resolve
+    console.log("Promise consumed");
+}); 
